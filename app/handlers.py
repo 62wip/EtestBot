2f59f499abc3f6ec5619924fc48f66d5ac66c946
62wip <5415044@mail.ru>
2023-10-08 19:11:19 +0300

    Start making command
    Create:
    + function for commans: start, how_to_use
    + start function: my_profile, create_test, solve_test, my_test, my_result, feedback

--- app/handlers.py
+++ app/handlers.py
@@ -11,16 +11,51 @@ router = Router()
 @router.message(Command('start'))
 async def start_command(message: Message):
     # Отправляем сообщение в ответ на команду /start
-    await message.answer('1', parse_mode="HTML")
+    await message.answer(f'Здравствуй,<u>{message.from_user.first_name}</u>! Это бот создан специально для <i>создания/решения тестов</i> онлайн. Чтобы узнать, как пользоваться ботом <i>пропиши команду</i> /how_to_use', parse_mode="HTML")
 
 # Обработчик команды /how_to_use
 @router.message(Command('how_to_use'))
 async def how_to_use_command(message: Message):
     # Отправляем сообщение в ответ на команду /how_to_use
-    await message.answer('2', parse_mode="HTML")
+    await message.answer('''1. Чтобы посмотреть статистику или изметить профиль, пропишите /my_profile
+2. Если вы хотите создать тест, пропришите /create_test
+3. Eсли вы хотите пройти тест, пропришите /solve_test и введите код теста(его можно получить у создателя теста)
+4. Прописав /my_test, вы сможете увидеть все созданные вами тесты
+5. Прописав /my_result, вы сможете увидеть все результаты тестов, которые вы решали
+6. Прописав /feedback, вы сможите оставить обратую связь''', parse_mode="HTML")
 
-# Обработчик команды /commands
-@router.message(Command('commands'))
+# Обработчик команды /my_profile
+@router.message(Command('my_profile'))
 async def commands_command(message: Message):
-    # Отправляем сообщение в ответ на команду /commands
-    await message.answer('3', parse_mode="HTML")
+    # Отправляем сообщение в ответ на команду /my_profile
+    await message.answer('my_profile', parse_mode="HTML")
+
+# Обработчик команды /create_test
+@router.message(Command('create_test'))
+async def commands_command(message: Message):
+    # Отправляем сообщение в ответ на команду /create_test
+    await message.answer('create_test', parse_mode="HTML")
+
+# Обработчик команды /solve_test
+@router.message(Command('solve_test'))
+async def commands_command(message: Message):
+    # Отправляем сообщение в ответ на команду /solve_test
+    await message.answer('solve_test', parse_mode="HTML")
+
+# Обработчик команды /my_test
+@router.message(Command('my_test'))
+async def commands_command(message: Message):
+    # Отправляем сообщение в ответ на команду /my_test
+    await message.answer('my_test', parse_mode="HTML")
+
+# Обработчик команды /my_result
+@router.message(Command('my_result'))
+async def commands_command(message: Message):
+    # Отправляем сообщение в ответ на команду /my_result
+    await message.answer('my_result', parse_mode="HTML")
+
+# Обработчик команды /feedback
+@router.message(Command('feedback'))
+async def commands_command(message: Message):
+    # Отправляем сообщение в ответ на команду /feedback
+    await message.answer('feedback', parse_mode="HTML")

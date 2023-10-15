2c0619815059a65eca681e0ab75caf1f0cc419c4
62wip <5415044@mail.ru>
2023-10-15 21:43:19 +0300

    Fixing startup and shutdown

--- run.py
+++ run.py
@@ -19,14 +19,17 @@ async def on_shutdown(dp: Dispatcher) -> None:
 
 # Определяем асинхронную функцию main
 async def main() -> None:
+    await bot.delete_webhook(drop_pending_updates=True) 
     # Создаем экземпляр бота с использованием API_TOKEN из настроек
     bot = Bot(token=API_TOKEN)
     # Создаем диспетчер (Dispatcher)
     dp = Dispatcher()
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
     # Включаем маршрутизатор (router), который будет обрабатывать входящие сообщения
     dp.include_router(router)
     # Запускаем бота в режиме long polling
-    await dp.start_polling(bot, on_startup=on_startup, on_shutdown=on_shutdown)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 if __name__ == '__main__':
     try:

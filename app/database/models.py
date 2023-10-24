3477ac183e17342b019272543fb51f56bb9d9759
62wip <5415044@mail.ru>
2023-10-24 17:38:34 +0300

    Small update

--- app/database/models.py
+++ app/database/models.py
@@ -9,9 +9,10 @@ class User():
         self.group = group
 
 class Test():
-    def __init__(self, creator_user_id: int, creation_time: datetime, test_name: str, subject_name: str, all_questions: list(str), all_answers: list(list(str)), right_answer: list(int), visible_result: bool) -> None:
+    def __init__(self, creator_user_id: int, creation_time: datetime, test_key: int,test_name: str, subject_name: str, all_questions: list(str), all_answers: list(list(str)), right_answer: list(int), visible_result: bool) -> None:
         self.creator_user_id = creator_user_id
         self.creation_time = creation_time
+        self.test_key = test_key
         self.test_name = test_name
         self.subject_name = subject_name
         self.all_questions = all_questions
